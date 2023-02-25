90028cbc53756f9f984099f27603182f022bbffa
Hanalam <lamtngochan@gmail.com>
2023-02-25 23:45:20 +0700

    customize toastify

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,14 @@ import './index.css';
 import { UserProvider } from './Context/User.context';
 import { ThemeProvider } from 'styled-components';
 import { themes } from './themes';
+import Toast from './components/Toast';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <ThemeProvider theme={themes}>
             <UserProvider>
                 <App />
+                <Toast />
             </UserProvider>
         </ThemeProvider>
     </React.StrictMode>
