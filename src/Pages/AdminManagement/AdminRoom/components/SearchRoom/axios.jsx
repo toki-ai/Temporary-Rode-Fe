4d60b54bb1c08e3c09cbb019ea9b317758658332
ngockim109 <ngockim9047@gmail.com>
2023-03-13 20:27:59 +0700

    api caller but not use

--- src/Pages/AdminManagement/AdminRoom/components/SearchRoom/axios.jsx
+++ src/Pages/AdminManagement/AdminRoom/components/SearchRoom/axios.jsx
@@ -1,10 +1,27 @@
 import axios from 'axios';
 
+import { get } from '../../../../../utils/ApiCaller';
+
 export const api = axios.create({
     baseURL: 'http://jsonplaceholder.typicode.com',
 });
-
 export const getInfos = async () => {
     const response = await api.get('/posts');
     return response.data;
 };
+
+// export const authApi = {
+//     getAllRoom: async () => {
+//         const endpoint = `/rooms/get-all`;
+//         // eslint-disable-next-line no-return-await
+
+//         return await get(endpoint, {}, {})
+//             .then((res) => {
+//                 // if (res.data.code !== 200) console.log(res.response);
+//                 return res;
+//             })
+//             .catch((err) => {
+//                 return err;
+//             });
+//     },
+// };
