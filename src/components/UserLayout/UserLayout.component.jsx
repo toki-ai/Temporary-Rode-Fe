0eccee7c094f1a7308238a7b2e04756669ed0635
Hanalam <lamtngochan@gmail.com>
2023-03-15 09:20:36 +0700

    Change background and import

--- src/components/UserLayout/UserLayout.component.jsx
+++ src/components/UserLayout/UserLayout.component.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import UserHeader from '../UserHeader/UserHeader.component';
+import { Container } from './styled';
 
 function UserLayoutComponent({ children }) {
     return (
-        <>
+        <Container>
             <UserHeader></UserHeader>
             {children}
-        </>
+        </Container>
     );
 }
 
