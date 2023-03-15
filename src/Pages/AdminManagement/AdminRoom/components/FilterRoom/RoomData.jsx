437d006a99aa625071bc7505867968054ec6cadf
ngockim109 <ngockim9047@gmail.com>
2023-03-15 11:05:09 +0700

    add empty style for table

--- src/Pages/AdminManagement/AdminRoom/components/FilterRoom/RoomData.jsx
+++ src/Pages/AdminManagement/AdminRoom/components/FilterRoom/RoomData.jsx
@@ -19,21 +19,27 @@ export function RoomData() {
                 console.log(err);
             });
     }, []);
-    return (
-        <>
-            {rooms.map((room) => (
-                <tr key={room.id}>
-                    <td>{room.code}</td>
-                    <td>{room.type}</td>
-                    <td>{DateFormat(room.openTime)}</td>
-                    <td>{room?.isPrivate ? DateFormat(room.closeTime) : <TfiInfinite />}</td>
-                    <td>{room?.isPrivate ? room.duration : <TfiInfinite />}</td>
-                    <td>{room?.isPrivate ? 'Private' : 'Public'}</td>
-                    <td>
-                        <More />
-                    </td>
-                </tr>
-            ))}
-        </>
+    const results = rooms.map((room) => (
+        <tr key={room.id}>
+            <td>{room.code}</td>
+            <td>{room.type}</td>
+            <td>{DateFormat(room.openTime)}</td>
+            <td>{room?.isPrivate ? DateFormat(room.closeTime) : <TfiInfinite />}</td>
+            <td>{room?.isPrivate ? room.duration : <TfiInfinite />}</td>
+            <td>{room?.isPrivate ? 'Private' : 'Public'}</td>
+            <td>
+                <More />
+            </td>
+        </tr>
+    ));
+    const content = results?.length ? (
+        results
+    ) : (
+        <tr>
+            <td className="text-center" colSpan={1000}>
+                No data available in table
+            </td>
+        </tr>
     );
+    return <>{content}</>;
 }
