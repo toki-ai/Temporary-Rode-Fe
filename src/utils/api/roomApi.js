af58e38261358fe417776a217477351e802cedbb
HaiDang <dangtranhai628@gmail.com>
2023-04-06 02:02:07 +0700

    UPDATE - Pull branch dev

--- src/utils/api/roomApi.js
+++ src/utils/api/roomApi.js
@@ -1,57 +1,16 @@
 import { post, get } from '../ApiCaller';
-import Localstorage from '../Localstorage';
-import authHeader from './HeaderAuthorization';
 
-const roomApi = {
-    getAllRoom: async (credential) => {
-        const endpoint = `/rooms/get-all`;
-        // eslint-disable-next-line no-return-await
-
-        return await get(endpoint, {}, authHeader())
-            .then((res) => {
-                // if (res.data.code !== 200) console.log(res.response);
-                return res;
-            })
-            .catch((err) => {
-                return err;
-            });
-    },
-    getRoomByCode: async (code) => {
-        const endpoint = `/rooms/get-one-by-code/${code}`;
-        return await get(endpoint, {}, authHeader())
-            .then((res) => {
-                return res;
-            })
-            .catch((err) => {
-                console.log('🚀 ~ file: roomApi.js:17 ~ getAll: ~ err:', err);
-                return err;
-            });
-    },
-    getRoomById: async (roomID) => {
-        const endpoint = `/rooms/get-one-by-id/${roomID}`;
-        return await get(endpoint, {}, authHeader())
-            .then((res) => {
-                return res;
-            })
-            .catch((err) => {
-                console.log('🚀 ~ file: roomApi.js:17 ~ getAll: ~ err:', err);
-                return err;
-            });
+const roomAPI = {
+    getAllRoomType: async () => {
+        const token = localStorage.getItem('token');
+        const endpoint = `/rooms/get-all-room-type`;
+        return await get(endpoint, {}, { Authorization: 'Bearer ' + token });
     },
     createOne: async (data) => {
         const token = localStorage.getItem('token');
         const endpoint = `/rooms/create-one`;
-        return await post(endpoint, data, {}, { Authorization: 'Bearer ' + token })
-            .then((res) => console.log(res))
-            .catch((err) => console.error(err));
-    },
-    getAllRoomType: async () => {
-        const token = localStorage.getItem('token');
-        const endpoint = `/rooms/get-all-room-type`;
-        return await get(endpoint, {}, { Authorization: 'Bearer ' + token })
-            .then((res) => res)
-            .catch((err) => console.error(err));
+        return await post(endpoint, data, {}, { Authorization: 'Bearer ' + token });
     },
 };
 
-export default roomApi;
+export default roomAPI;
